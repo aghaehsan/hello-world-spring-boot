1a776998ac9a2939051a808d079dd60a9111dd1d
ehan <ehan@ehans-mbp.attlocal.net>
2019-08-09 22:55:06 -0500

    modified

--- provScripts/installTomcat.sh
+++ provScripts/installTomcat.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -xe
 sudo amazon-linux-extras install tomcat8.5 && sudo chkconfig tomcat on
 cd ~
-curl -O https://tomcat.apache.org/tomcat-7.0-doc/appdev/sample/sample.war
-sudo chmod 555 sample.war
-sudo mv sample.war /usr/share/tomcat/webapps/
+mv /tmp/*.war .
+sudo chmod 555 *.war
+sudo mv *.war /usr/share/tomcat/webapps/
+
